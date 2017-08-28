404bfda8bcdd59d1b395a1c527b225c2c33778db
Brad Efting <bradefting@gmail.com>
2017-08-28 11:20:54 -0600

    Add . code

--- public/script/app.js
+++ public/script/app.js
@@ -1,6 +1,6 @@
 var myApp = angular.module("myApp", ['ui.bootstrap', 'ngRoute', 'angulartics', 'angulartics.google.tagmanager']);
 
-myApp.config(function($locationProvider, $routeProvider){
+myApp.config(function($locationProvider, $routeProvider, $rootScope,  $location){
 
     // add this after d. injection above
     // for html5Mode
@@ -8,6 +8,13 @@ myApp.config(function($locationProvider, $routeProvider){
     // $locationProvider.hashPrefix('!');
     // for hashbang mode
     // $locationProvider.html5Mode(false).hashPrefix('!');
+
+    $rootScope.$on('$stateChangeSuccess', function (event, toState) {
+        window.dataLayer.push({
+            event: 'pageView',
+            action: $location.path(),
+        });
+    });
 
     $routeProvider
 
